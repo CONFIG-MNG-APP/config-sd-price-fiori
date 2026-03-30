91e6ecf4ea76414a0a1420de366423d21935a3d8
Quy Hoang <mausuper202@gmail.com>
2026-03-30 17:52:04 +0700

    app sd price version 1

---
+++ dist/test/integration/pages/SDPriceConfMain.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/test/TemplatePage"],function(e){"use strict";var n={actions:{},assertions:{}};return new e("zgsp26.conf.mng.sdprice.confmngfesdprice::SDPriceConfMain",n)});
+//# sourceMappingURL=SDPriceConfMain.js.map
